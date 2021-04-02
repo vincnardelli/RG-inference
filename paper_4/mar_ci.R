5b29d8690196ba321ed6ee691e8696d02526b21f
Vincenzo Nardelli <vincnardelli@gmail.com>
2021-04-02 14:57:52 +0200

    update

--- paper_4/mar_ci.R
+++ paper_4/mar_ci.R
@@ -43,9 +43,9 @@ data_generation_step2 <- function(data, prop=0){
 
 
 #parametri simulazione
-n_dataset <- 2
-props <- seq(0, .9, by=0.1)
-n_sim <- 1:500
+n_dataset <- 100
+props <- seq(0.1, 0.9, by=0.2)
+n_sim <- 1:200
 tts = c(0.1, 0.9)
 
 # genero dataset
@@ -94,41 +94,46 @@ simulazione$beta2=unlist(sapply(simulazione_lista, function(x) x[3]))
 simulazione$beta3=unlist(sapply(simulazione_lista, function(x) x[4]))
 
 # real values
-sim_real <- function(dataset, tt){
+sim_real <- function(dataset_id, tt){
   
-  dataset_id <- which(dataset_list$dataset==dataset & dataset_list$prop == 0)
-  data <- data_list[[dataset_id]]
+  data <- dataset[[dataset_id]]
   #q-regression on non-validation dataset Y ~ Xhat + Z
   model <- boot.rq(x=cbind(1,data[,2:4]), y=data[,1], tau = tt, bsmethod= "xy", R=500)
-  t(apply(model$B, 2, quantile, c(0.025,0.975)))
+  list(t(apply(model$B, 2, quantile, c(0.025, 0.975))),
+  t(apply(model$B, 2, mean)))
 }
 
 simulazione_real <- expand.grid(dataset = 1:n_dataset,
                                 tt = tts)
 
 simulazione_real_lista <- parallel::mclapply(1:nrow(simulazione_real), 
-                                             function(x) sim_real(dataset = simulazione_real[x, ]$dataset,
+                                             function(x) sim_real(dataset_id = simulazione_real[x, ]$dataset,
                                                                   tt=simulazione_real[x, ]$tt), 
                                              mc.cores = parallel::detectCores()-1)
 
-simulazione_real$beta0_lower=unlist(sapply(simulazione_real_lista, function(x) x[1,1]))
-simulazione_real$beta0_upper=unlist(sapply(simulazione_real_lista, function(x) x[1,2]))
+simulazione_real$beta0_lower=unlist(sapply(simulazione_real_lista, function(x) x[[1]][1,1]))
+simulazione_real$beta0_mean=unlist(sapply(simulazione_real_lista, function(x) x[[2]][1]))
+simulazione_real$beta0_upper=unlist(sapply(simulazione_real_lista, function(x) x[[1]][1,2]))
 
-simulazione_real$beta1_lower=unlist(sapply(simulazione_real_lista, function(x) x[2,1]))
-simulazione_real$beta1_upper=unlist(sapply(simulazione_real_lista, function(x) x[2,2]))
+simulazione_real$beta1_lower=unlist(sapply(simulazione_real_lista, function(x) x[[1]][2,1]))
+simulazione_real$beta1_mean=unlist(sapply(simulazione_real_lista, function(x) x[[2]][2]))
+simulazione_real$beta1_upper=unlist(sapply(simulazione_real_lista, function(x) x[[1]][2,2]))
 
-simulazione_real$beta2_lower=unlist(sapply(simulazione_real_lista, function(x) x[3,1]))
-simulazione_real$beta2_upper=unlist(sapply(simulazione_real_lista, function(x) x[3,2]))
+simulazione_real$beta2_lower=unlist(sapply(simulazione_real_lista, function(x) x[[1]][3,1]))
+simulazione_real$beta2_mean=unlist(sapply(simulazione_real_lista, function(x) x[[2]][3]))
+simulazione_real$beta2_upper=unlist(sapply(simulazione_real_lista, function(x) x[[1]][3,2]))
 
-simulazione_real$beta3_lower=unlist(sapply(simulazione_real_lista, function(x) x[4,1]))
-simulazione_real$beta3_upper=unlist(sapply(simulazione_real_lista, function(x) x[4,2]))
+simulazione_real$beta3_lower=unlist(sapply(simulazione_real_lista, function(x) x[[1]][4,1]))
+simulazione_real$beta3_mean=unlist(sapply(simulazione_real_lista, function(x) x[[2]][4]))
+simulazione_real$beta3_upper=unlist(sapply(simulazione_real_lista, function(x) x[[1]][4,2]))
 
 
 simulazione_real <- simulazione_real %>% 
   pivot_longer(beta0_lower:beta3_upper, names_to = "parameter", values_to = "rq") %>%
   rowwise() %>%
   mutate(bound = strsplit(parameter, "_")[[1]][2], 
-        parameter =strsplit(parameter, "_")[[1]][1])
+        parameter =strsplit(parameter, "_")[[1]][1]) %>%
+  pivot_wider(names_from = bound, values_from = rq)
 
 
 save(simulazione_real, simulazione, file="paper_4/simulazione.RData")
@@ -140,24 +145,48 @@ load("paper_4/simulazione.RData")
 risultati <- simulazione %>% 
   pivot_longer(beta0:beta3, names_to = "parameter") %>% 
   group_by(dataset, prop, tt, parameter) %>%
-  summarise(#estimate=mean(value), 
-            lower = mean(value)-1.96*sd(value), 
-            upper = mean(value)+1.96*sd(value)) %>% 
-  pivot_longer(lower:upper, names_to="bound", values_to = "estimate") %>%
-  left_join(simulazione_real, by=c("dataset", "tt", "parameter", "bound")) %>%
+  summarise(boot_mean=mean(value), 
+            boot_lower = mean(value)-1.96*sd(value), 
+            boot_upper = mean(value)+1.96*sd(value)) %>% 
+  left_join(simulazione_real, by=c("dataset", "tt", "parameter")) %>%
   filter(prop != 0) %>%
+  mutate(inside_bootstrap = ifelse(mean > boot_lower & mean < boot_upper, T, F))
+
+risultati <- simulazione %>% 
+  pivot_longer(beta0:beta3, names_to = "parameter") %>% 
   group_by(dataset, prop, tt, parameter) %>%
-  pivot_wider(names_from = bound, values_from = estimate:rq)
+  summarise(boot_mean=mean(value), 
+            boot_lower = quantile(value, 0.025), 
+            boot_upper = quantile(value, 0.975)) %>% 
+  left_join(simulazione_real, by=c("dataset", "tt", "parameter")) %>%
+  filter(prop != 0) %>%
+  mutate(inside_bootstrap = ifelse(mean > boot_lower & mean < boot_upper, T, F))
 
 fig1 <- risultati %>%
   filter(dataset == 1) %>%
 ggplot() + 
   geom_ribbon(aes(x= prop, 
-                  ymin = estimate_lower, 
-                  ymax = estimate_upper), fill="gray80") +
-  geom_hline(aes(yintercept = rq_lower)) +
-  geom_hline(aes(yintercept = rq_upper)) +
+                  ymin = boot_lower, 
+                  ymax = boot_upper), fill="gray80") +
+  geom_hline(aes(yintercept = lower)) +
+  geom_hline(aes(yintercept = upper)) +
   facet_grid(cols = vars(parameter), rows = vars(tt), scales = "free", space="free") +
   theme_bw()
 fig1
 ggsave("paper_4/ci_bootstrap_fig1.pdf", fig1, height = 5, width = 8)
+
+
+
+fig2 <- risultati %>% 
+  group_by(prop, tt, parameter) %>% 
+  summarise(coverage = mean(inside_bootstrap)) %>% 
+  ggplot() +
+  geom_line(aes(prop, coverage)) +
+  facet_grid(rows=vars(tt), cols=vars(parameter)) +
+  theme_bw()  +
+  ylab("Copertura") +
+  xlab("Prop") +
+  ggtitle("Copertura CI")
+fig2
+ggsave("paper_4/prop.pdf", fig2, height = 5, width = 8)
+
